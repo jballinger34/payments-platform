fed0b0d48e5868e8b436dcd131e6f6abdc36c136
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-05-19 12:35:04 +0100

    Added domain model objects

---
+++ src/main/java/me/jamie/fullstack_payments_transaction_platform/model/Currency.java
@@ -0,0 +1,10 @@
+package me.jamie.fullstack_payments_transaction_platform.model;
+
+public enum Currency {
+
+    GBP,
+    USD,
+    ;
+
+
+}
---
+++ src/main/java/me/jamie/fullstack_payments_transaction_platform/model/Payment.java
@@ -0,0 +1,62 @@
+package me.jamie.fullstack_payments_transaction_platform.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+public class Payment {
+
+    @Id
+    private String id;
+    private String payerName;
+    private String payeeName;
+    private BigDecimal amount;
+    private Currency currency;
+    private PaymentStatus status;
+
+    public Payment() {}
+
+    public Payment(String payerName, String payeeName, BigDecimal amount, Currency currency) {
+        this(String.valueOf(UUID.randomUUID()), payerName, payeeName, amount, currency, PaymentStatus.INITIATED);
+    }
+
+    public Payment(String id, String payerName, String payeeName, BigDecimal amount, Currency currency, PaymentStatus status) {
+        this.id = id;
+        this.payerName = payerName;
+        this.payeeName = payeeName;
+        this.amount = amount;
+        this.currency = currency;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPayerName() {
+        return payerName;
+    }
+
+    public String getPayeeName() {
+        return payeeName;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+}
